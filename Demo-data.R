e7069a3f15f0396ea15c1af2aa8f50eef147dc8a
sabrina-xie <sabrina.xie@dec.ny.gov>
2023-01-25 14:35:33 -0500

    added percentile ranks and E3b equivalent thresholds to racial diversity

--- Demo-data.R
+++ Demo-data.R
@@ -1,24 +1,24 @@
-Energy_burden <- read.csv("C:/Users/xiesa/OneDrive - State of Connecticut/2019-2021 Equitable/Energy_burden.csv") %>% 
+Energy_burden <- read.csv("Energy_burden.csv") %>% 
   select(Census.tract,Energy.burden) %>% 
   rename(`Census Tract`=Census.tract) %>% 
   mutate(Energy.burden=toupper(Energy.burden))
 
 data <- left_join(data,Energy_burden,by="Census Tract")
 
-Mod_distress <- read.csv("C:/Users/xiesa/OneDrive - State of Connecticut/2019-2021 Equitable/Mod_distress.csv") %>% 
+Mod_distress <- read.csv("Mod_distress.csv") %>% 
   rename(`Census Tract`=Census.Tract) %>% 
   mutate(Moderate.distress=toupper(Moderate.distress))
 
 data <- left_join(data,Mod_distress,by="Census Tract")
 
-Racial_diversity <- read.csv("C:/Users/xiesa/OneDrive - State of Connecticut/2019-2021 Equitable/Racial_diversity.csv")%>% 
+Racial_diversity <- read.csv("Racial_diversity.csv")%>% 
   select(tract,percent) %>% 
   rename(`Census Tract`=tract,
          Racial.diversity=percent)
 
 data <- left_join(data,Racial_diversity,by="Census Tract")
 
-J40 <- read.csv("C:/Users/xiesa/OneDrive - State of Connecticut/2019-2021 Equitable/Justice40.csv") 
+J40 <- read.csv("Justice40.csv") 
 
 Justice40 <- J40 %>% 
   select(Census.tract.2010.ID,Identified.as.disadvantaged) %>% 
